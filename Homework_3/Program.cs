bc86fc3db1b245560df15d9b5c78b86cfcc8e568
Izillger <44162880+Izillger@users.noreply.github.com>
2020-12-06 17:26:01 +0700

    Добавьте файлы проекта.

---
+++ Homework_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+    {
+    class Program
+    {
+        #region // Переменные
+        private string user1, user2, comp; // Переменные для двух игроков и компьютера
+        private bool hod;                  // Для передачи хода
+        Random gameNumber = new Random();  // Для рандомного числа
+        private int userTry;               // Для ввода числа игроками
+        #endregion
+
+        static void Main(string[] args)
+            {
+            }
+        }
+    }
